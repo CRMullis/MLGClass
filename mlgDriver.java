b36a1881774af967ab016e7b21c63d096e04c111
Collin Mullis <cmullis@saintjoe.edu>
2015-02-13 17:28:43 -0500

    Added Scumper's profile to Program and made Driver

---
+++ mlgDriver.java
@@ -0,0 +1,7 @@
+public class mlgDriver {
+	public static void main(String[] args) {
+		mlg opticScump = new mlg("Optic", "Scumper", 1.24, 7.48, 2.58);
+		System.out.println(opticScump.toString());
+		}	
+	}
+				
